2d7dc1bce505500c51ed7fb9c84223532a484801
Felipe Santaniello <felipe.santaniello-ext@viavarejo.com.br>
2019-08-20 09:56:39 -0300

    Commit inicial

---
+++ src/main/java/com/example/multidatabases/MultidatabasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.multidatabases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidatabasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultidatabasesApplication.class, args);
+	}
+
+}
